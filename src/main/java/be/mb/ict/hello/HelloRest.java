749baf90c0921a31af1436fe2f0889a3d312d92a
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2018-10-22 21:08:28 +0200

    Initial project

---
+++ src/main/java/be/mb/ict/hello/HelloRest.java
@@ -0,0 +1,12 @@
+package be.mb.ict.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRest {
+	
+    public static void main(String[] args) {
+    	SpringApplication.run(HelloRest.class, args);
+    }
+}
