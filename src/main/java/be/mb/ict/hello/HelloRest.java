e2dfd8066b50c6a2fc65404fc9f18d9b988d9ad0
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2018-11-04 21:24:33 +0100

    Enable infinispan caching

--- src/main/java/be/mb/ict/hello/HelloRest.java
+++ src/main/java/be/mb/ict/hello/HelloRest.java
@@ -1,8 +1,11 @@
 package be.mb.ict.hello;
 
+import org.infinispan.manager.DefaultCacheManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableCaching
@@ -11,4 +14,7 @@ public class HelloRest {
     public static void main(String[] args) {
     	SpringApplication.run(HelloRest.class, args);
     }
+
 }
+
+
