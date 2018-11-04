0b871f676f9beaec122c70e78ec791d9775a025e
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2018-11-04 20:57:58 +0100

    Cache greetings

--- src/main/java/be/mb/ict/hello/GreetingService.java
+++ src/main/java/be/mb/ict/hello/GreetingService.java
@@ -1,5 +1,6 @@
 package be.mb.ict.hello;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,6 +8,7 @@ public class GreetingService {
 	
 	private static int greetingId = 1;
 
+	@Cacheable("greetings")
 	public Greeting greet(String name) {
 		return new Greeting(greetingId++, name);
 	}
--- src/main/java/be/mb/ict/hello/HelloRest.java
+++ src/main/java/be/mb/ict/hello/HelloRest.java
@@ -2,8 +2,10 @@ package be.mb.ict.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class HelloRest {
 	
     public static void main(String[] args) {
